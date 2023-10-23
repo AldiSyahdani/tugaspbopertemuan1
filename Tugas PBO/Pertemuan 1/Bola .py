2341144029ea58f9367adb83c569b60827762782
AldiSyahdani <148462517+AldiSyahdani@users.noreply.github.com>
2023-10-23 12:46:26 +0700

    Add files via upload

---
+++ Tugas PBO/Pertemuan 1/Bola .py
@@ -0,0 +1,14 @@
+print ("Aplikasi menghitung luas dan volume Bola")
+
+# Atur nilai variabel
+JariJari = 5
+
+# Rumus
+luas = 4 * 3.14 * (JariJari*JariJari)
+volume = 4/3 * 3.14 * (JariJari*JariJari*JariJari)
+
+# Output
+print("Jari Jari = ",JariJari)
+print("Hasilnya Adalah ")
+print("Luas =",luas)
+print("Volume =",volume)
---
+++ Tugas PBO/Pertemuan 1/Limas Segiempat .py
@@ -0,0 +1,18 @@
+print ("Aplikasi menghitung luas dan volume Limas Segiempat")
+
+# Atur nilai variabel
+LuasSisi = 6
+LuasAlas = 5
+Tinggi = 9
+
+# Rumus
+luas = LuasSisi + LuasSisi + LuasSisi + LuasSisi + LuasSisi
+volume = LuasAlas * Tinggi / 3
+
+# Output
+print("Luas sisi= ",LuasSisi)
+print("Luas alas= ",LuasAlas)
+print("Tinggi= ",Tinggi)
+print("Hasilnya Adalah")
+print("Luas =",luas)
+print("Volume =",volume)
